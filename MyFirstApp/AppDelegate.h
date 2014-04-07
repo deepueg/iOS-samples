29732cd31fccc49824425d669d50193ddba8074b
Deepu Eswarath Ganapathiyadan <dganapathiya@paypal.com>
2014-04-07 09:45:08 -0700

    Initial Commit

---
+++ MyFirstApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by Ganapathiyadan, Deepu on 4/7/14.
+//  Copyright (c) 2014 Paypal Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyFirstApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyFirstApp
+//
+//  Created by Ganapathiyadan, Deepu on 4/7/14.
+//  Copyright (c) 2014 Paypal Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
