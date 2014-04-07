3db1a044863c9d67221f03329a28e6055c308ee8
Deepu Eswarath Ganapathiyadan <dganapathiya@paypal.com>
2014-04-07 13:19:46 -0700

    Added property definitions.

--- MyFirstApp/Person.h
+++ MyFirstApp/Person.h
@@ -10,8 +10,27 @@
 
 @interface Person : NSObject
 
-@property NSString *name;
-@property int age;
+//Default is (atomic, readonly, strong) For objects
+//Default is (atomic, readonly, assign) For simple types.
+
+//readonly: only generates the getter.
+//readwite: generates both getter and setter.
+
+//use assign on simple types. Dont use it on objects.
+
+//Use atomic wisely, since calling getter ir setter on an atomic var will stop the main thread until the operation is completed.
+
+//Don't use unsafe_unretained, its deprecated. unsafe_unretained means give me the reference but I dont own it.
+
+//weak and unsafe_unretained does the same thing, but the difference is weak sets the reference to nil.
+
+//copy: use it when your class is mutable. That means when you assign a value to a 'copy' variable it makes an immutable copy and assigns it.
+
+@property (nonatomic, assign) int age;
+
+
+
+@property (nonatomic, strong) NSString *name; //default is atomic.
 
 - (id)initWithName:(NSString *)name age:(int)age;
 - (void)walk;
